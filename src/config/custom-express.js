94f03ba22ba000a2ae8bc5a2fabcbed83d17daa2
bellsgrimaldi <isabellagrimaldi10@gmail.com>
2020-11-17 21:37:52 -0300

    Livraria
    Livraria - Cadastro de Livros

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -3,6 +3,11 @@ require('marko/express');
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ //informa que está habilitado para receber objetos complexos em formato json
+    extended: true
+}));
 
 const rotas = require('../app/rotas/rotas.js');
 
